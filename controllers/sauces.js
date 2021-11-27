2e0173939455eca3222879f49730b0c06e7fe258
JulienPetit <julienpetit1@gmail.com>
2021-11-27 19:54:22 +0100

    fonction like

--- controllers/sauces.js
+++ controllers/sauces.js
@@ -52,7 +52,7 @@ exports.deleteThing = (req, res, next) => {
     .catch(error => res.status(500).json({ error }));
 };
 
-exports.getAllStuff = (req, res, next) => {
+exports.getAllThing = (req, res, next) => {
   Thing.find()
     .then((things) => {
       res.status(200).json(things);
@@ -62,4 +62,45 @@ exports.getAllStuff = (req, res, next) => {
         error: error,
       });
     });
+};
+
+exports.likeThing = (req, res, next) => {
+  const like = req.body.like;
+  const idSauce = req.params.id;
+  
+  Thing.findOne({ _id: idSauce })
+  .then (thing => {
+      const idIncluded = !thing.usersLiked.includes(req.body.userId) && !thing.usersDisliked.includes(req.body.userId);
+      if( like === 1 && idIncluded ) {
+          Thing.updateOne({ _id:idSauce },{
+              $push: { usersLiked: req.body.userId },
+              $inc: { likes: +1 }
+          })
+          .then(() => res.status(200).json({ message: 'like ajouté !'}))
+          .catch(error => res.status(400).json({ error }));
+      }else if( like === -1 && idIncluded ) {
+          Thing.updateOne({ _id:idSauce }, {
+              $push: { usersDisliked: req.body.userId },
+              $inc: { dislikes: +1 }
+          })
+          .then(() => res.status(200).json({ message: 'dislike ajoutée !'}))
+          .catch(error => res.status(400).json({ error }));
+      }else {
+          if(thing.usersLiked.includes(req.body.userId)){
+              Thing.updateOne({ _id:idSauce },{
+                  $pull: { usersLiked: req.body.userId },
+                  $inc: { likes: -1 }
+              }) 
+              .then(() => res.status(200).json({ message: 'like retiré !'}))
+              .catch(error => res.status(400).json({ error })); 
+          }else if(thing.usersDisliked.includes(req.body.userId)){
+              Thing.updateOne({ _id:idSauce }, {
+                  $pull: { usersDisliked: req.body.userId },
+                  $inc: { dislikes: -1 } 
+              })
+              .then(() => res.status(200).json({ message: 'dislike retiré !'}))
+              .catch(error => res.status(400).json({ error }));
+          }
+      }
+  })
 };
--- routes/sauces.js
+++ routes/sauces.js
@@ -6,10 +6,12 @@ const multer = require('../middleware/multer-config');
 
 const saucesCtrl = require('../controllers/sauces');
 
-router.get('/', auth, multer, saucesCtrl.getAllStuff);
-router.post('/', auth, saucesCtrl.createThing);
-router.get('/:id', auth, multer, saucesCtrl.getOneThing);
+
+router.post('/', auth, multer, saucesCtrl.createThing);
+router.post('/:id/like', auth, multer, saucesCtrl.likeThing);
 router.put('/:id', auth, multer, saucesCtrl.modifyThing);
 router.delete('/:id', auth, saucesCtrl.deleteThing);
+router.get('/:id', auth, saucesCtrl.getOneThing);
+router.get('/', auth, saucesCtrl.getAllThing);
 
 module.exports = router;
