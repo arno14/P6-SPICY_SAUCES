1b8381f3cca0322025e3eecb10336a397fbd1f20
Arnaud <arnolepeltier.dev@gmail.com>
2021-11-28 23:21:28 +0100

    configuration de l'application via env files

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const express = require("express");
 const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
 const path = require('path');
+require('dotenv').config();
 
 const userRoutes = require("./routes/user");
 const saucesRoutes = require('./routes/sauces');
 
 mongoose.connect(
-    "mongodb+srv://JuliusMinus:Chercheur.93@cluster0.mszxr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    process.env.MONGO_CONNECTION,
     { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(() => console.log("Connexion à MongoDB réussie !"))
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const http = require('http');
 const app = require('./app');
 
+require('dotenv').config();
+
 const normalizePort = val => {
   const port = parseInt(val, 10);
 
@@ -12,7 +14,7 @@ const normalizePort = val => {
   }
   return false;
 };
-const port = normalizePort(process.env.PORT || '3000');
+const port = normalizePort(process.env.SERVER_PORT);
 app.set('port', port);
 
 const errorHandler = error => {
